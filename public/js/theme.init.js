bab995f129f8ff6eebfab989fff58182521a0c67
jpg-consulting <opensource@jpg-consulting.es>
2016-01-16 01:49:46 +0100

    Added scroll-to-top jquery plugin

---
+++ public/js/theme.init.js
@@ -0,0 +1,11 @@
+// Commom Plugins
+(function($) {
+
+	'use strict';
+
+	// Scroll to Top Button.
+	if (typeof theme.PluginScrollToTop !== 'undefined') {
+		theme.PluginScrollToTop.initialize();
+	}
+
+}).apply(this, [jQuery]);
