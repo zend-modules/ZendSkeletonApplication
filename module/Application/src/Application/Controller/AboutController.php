d15b9baf940be852a1cb79e42838c412242b4da9
jpg-consulting <opensource@jpg-consulting.es>
2016-01-16 01:17:04 +0100

    Added schemas for About and Contact pages

--- module/Application/src/Application/Controller/AboutController.php
+++ module/Application/src/Application/Controller/AboutController.php
@@ -2,12 +2,23 @@
 namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\HelperPluginManager as ViewHelperPluginManager;
 use Zend\View\Model\ViewModel;
 
 class AboutController extends AbstractActionController
 {
     public function indexAction()
     {
+        $services = $this->getServiceLocator();
+        $viewHelperManager = $services->get('ViewHelperManager');
+        if ($viewHelperManager instanceof ViewHelperPluginManager) {
+            // htmlTag view helper
+            if ( $viewHelperManager->has('htmlTag') ) {
+                $htmlTag = $viewHelperManager->get('htmlTag');
+                $htmlTag->setAttribute('itemtype', 'http://schema.org/AboutPage');
+            }
+        }
+
         return new ViewModel();
     }
 }
--- module/Application/src/Application/Controller/ContactController.php
+++ module/Application/src/Application/Controller/ContactController.php
@@ -2,12 +2,23 @@
 namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\HelperPluginManager as ViewHelperPluginManager;
 use Zend\View\Model\ViewModel;
 
 class ContactController extends AbstractActionController
 {
     public function indexAction()
     {
+        $services = $this->getServiceLocator();
+        $viewHelperManager = $services->get('ViewHelperManager');
+        if ($viewHelperManager instanceof ViewHelperPluginManager) {
+            // htmlTag view helper
+            if ( $viewHelperManager->has('htmlTag') ) {
+                $htmlTag = $viewHelperManager->get('htmlTag');
+                $htmlTag->setAttribute('itemtype', 'http://schema.org/ContactPage');
+            }
+        }
+
         return new ViewModel();
     }
 }
